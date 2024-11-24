1286b9ceee4ff17e2981805d7f430527739acd8c
soong7 <seungah715@naver.com>
2024-11-24 11:35:01 +0900

    스포티파이 API 폴더 삭제
    main 브랜치에서
    server 브랜치로 이동

--- spotify_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.spotify_login, name='spotify-login'),
-    path('callback/', views.spotify_callback, name='spotify-callback'),
-]
-
